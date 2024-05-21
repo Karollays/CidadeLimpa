a58766bf189c3d7e2d3ea13951f24ed37ee93bd2
Emilio Bringhenti Junior <emiliobringjr@gmail.com>
2024-05-20 21:18:09 -0300

    inicial commit

--- src/main/java/br/com/fiap/cidadelimpa/CidadelimpaApplication.java
+++ src/main/java/br/com/fiap/cidadelimpa/CidadelimpaApplication.java
@@ -1,4 +1,4 @@
-package br.com.fiap.cidadelimpa.cidadelimpa;
+package br.com.fiap.cidadelimpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/br/com/fiap/cidadelimpa/model/Coleta.java
@@ -0,0 +1,4 @@
+package br.com.fiap.cidadelimpa.model;
+
+public class Coleta {
+}
