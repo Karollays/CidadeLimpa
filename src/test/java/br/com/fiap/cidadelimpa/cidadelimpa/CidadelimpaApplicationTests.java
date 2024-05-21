b827dcb31539a41513da96c3d4c5592ef26f0167
Karollays <karol.penteadoalves@gmail.com>
2024-05-20 21:02:27 -0300

    Initial commit

---
+++ src/test/java/br/com/fiap/cidadelimpa/cidadelimpa/CidadelimpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.cidadelimpa.cidadelimpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadelimpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
