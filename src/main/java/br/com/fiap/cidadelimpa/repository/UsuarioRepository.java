076232e81905378d898b42868f42b0a58019f2cc
Adriano <adrianokim@gmail.com>
2024-05-22 21:31:56 -0300

    Exemplo do Security

---
+++ src/main/java/br/com/fiap/cidadelimpa/repository/UsuarioRepository.java
@@ -0,0 +1,20 @@
+package br.com.fiap.cidadelimpa.repository;
+
+import br.com.fiap.cidadelimpa.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query("SELECT c FROM Contato c WHERE c.nome = :nome")
+    Optional<Usuario> findByNome(@Param("nome") String nome);
+
+
+//    UserDetails findByNome(String nome);
+    UserDetails findByEmail(String email);
+
+}
