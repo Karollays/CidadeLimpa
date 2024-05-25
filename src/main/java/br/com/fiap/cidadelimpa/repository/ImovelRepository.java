fb28fbdcb6571237a632501c5692643e6d3489af
Adriano <adrianokim@gmail.com>
2024-05-25 13:11:45 -0300

    Mudando o query Contato para Usuario

--- src/main/java/br/com/fiap/cidadelimpa/repository/ImovelRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.cidadelimpa.repository;
-
-import br.com.fiap.cidadelimpa.model.Imovel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImovelRepository extends JpaRepository<Imovel, Long> {}
--- src/main/java/br/com/fiap/cidadelimpa/repository/MoradorRepository.java
+++
@@ -1,6 +0,0 @@
-package br.com.fiap.cidadelimpa.repository;
-
-import br.com.fiap.cidadelimpa.model.Morador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MoradorRepository extends JpaRepository<Morador, Long> {}
